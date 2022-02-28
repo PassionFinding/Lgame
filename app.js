24889ab9f5282fe226ae8cbfc9abbb06ad89719f
PassionFinding <JustUserAM@gmail.com>
2022-02-27 20:11:27 -0800

    Work on the click and drag move functionality

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ const square13 = document.getElementById('13')
 const square14 = document.getElementById('14')
 const square15 = document.getElementById('15')
 const square16 = document.getElementById('16')
+const table = document.querySelector('table')
 
 const squares = [square1, square2, square3, square4, square5, square6, square7, square8, square9, square10, square11, square12, square13, square14, square15, square16]
+const red_squares = [square2, square3, square7, square11]
+const blue_squares = [square6, square10, square14, square15]
 
 //true is blue turn, false is red turn
 let turn = false;
@@ -32,14 +35,51 @@ let turn = false;
 //     }
 // }
 
-squares.forEach(function(square) {
-    if (turn === true && !square.firstChild) {
-        square.addEventListener('click', function(event) {
-            event.target.style.backgroundColor = 'blue';
-        })
-    } else if (turn === false && !square.firstChild) {
-        square.addEventListener ('click', function(event) {
-            event.target.style.backgroundColor = 'red';
+// squares.forEach(function(square) {
+//     const style = getComputedStyle(square)
+//     const color = style.backgroundColor
+//     if (turn === true && !square.firstChild) {
+//         square.addEventListener('click', function(event) {
+//             event.target.style.backgroundColor = 'blue';
+//         })
+//     } else if (turn === false) {
+//         square.addEventListener ('click', function(event) {
+//             event.target.innerHTML = event.target.style.backgroundColor
+//         })
+//     }
+// })
+
+const movement = function(event){
+    if (turn === false) {
+        red_squares.forEach(item => item.style.backgroundColor = 'lightcoral')
+        event.target.style.backgroundColor = 'red'
+        squares.forEach(function(square){
+            const style = getComputedStyle(square)
+            const color = style.backgroundColor
+            if ((color === "rgb(255, 255, 255)" || color === "rgb(240, 128, 128)") && !square.firstChild) {
+                square.addEventListener('mouseover', function(event){
+                    event.target.style.backgroundColor = 'red'
+                })
+            }
         })
     }
-})
+}
+
+const unmovement = function(event){
+    if (turn === false) {
+        squares.forEach(function(square){
+            const style = getComputedStyle(square)
+            const color = style.backgroundColor
+            if (color === "rgb(255, 0, 0)") {
+                square.style.backgroundColor = 'white'
+                square.removeEventListener('mouseover', function(event){
+                    event.target.style.backgroundColor = 'red'
+                })
+            }
+        })
+        red_squares.forEach(item => item.style.backgroundColor = 'red')
+    }
+}
+
+table.addEventListener('mousedown', movement)
+table.addEventListener('mouseup', unmovement)
